53e03a674f9651568ec0c9685073667388cc5a87
xilder <olabowaleayobami@gmail.com>
2024-02-16 14:37:03 +0100

    moved them

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-imports all models created and
-creates a storage engine model
-"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
