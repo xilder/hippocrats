fca3aa1cc4fb7c46fa28ed273e9a05a346ab9f9d
xilder <olabowaleayobami@gmail.com>
2024-02-18 01:40:43 +0100

    added login function

--- hippocrat/src/pages/login/login.jsx
+++ hippocrat/src/pages/login/login.jsx
@@ -1,32 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import httpClient from "../../utils/httpClient";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import "./login.scss";
 
 const Login = () => {
-    return (
-        <div>
-            <header className='logo'>
-                <img src='../assets/img.jpg' alt='Logo'/>
-                {/* <nav className='navbar'>
-                        <a href='#'>Login/Register</a>
-                        <a href='#'>About Us</a>
-                        <a href='#'>Contact Us</a>
-                </nav> */}
-            </header>
-            <div>
-                <fieldset>
-                    <legend>Login</legend>
-                    <label>
-                        <span>Username/Email:</span>
-                        <input type='text' name='username' id='username' placeholder='Lagbaja Tamedu' required />
-                    </label>
-                    <label>
-                        <span>Password:</span>
-                        <input type='password' name='password' id='password' placeholder='Enter Password' required />
-                    </label>
-                    <input type='submit' name='submit'/>
-                </fieldset>
-            </div>
-        </div>
-    )
-}
+  const [data, setData] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const loginUser = async () => {
+    try {
+      const response = await httpClient.post("//localhost:5000/api/v1/login", {
+        data,
+        password,
+      });
+      console.log(response)
+    } catch (e) {
+      alert(e.response.data.error);
+    }
+  };
+
+  return (
+    <div className="login">
+      <Header />
+      <div className="login-form">
+        <form>
+          <fieldset>
+            <legend>Login</legend>
+            <label>
+              <span>Username/Email:</span>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Lagbaja Tamedu"
+                onChange={(e) => setData(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              <span>Password:</span>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter Password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <button type="button" className="submit" onClick={loginUser}>
+              Login
+            </button>
+          </fieldset>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
