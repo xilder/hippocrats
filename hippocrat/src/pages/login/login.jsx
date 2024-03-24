6fd1382414088f9410459167b46d0920d0cd6112
xilder <olabowaleayobami@gmail.com>
2024-03-24 03:04:19 +0100

    style changes

--- hippocrat/src/pages/login/login.jsx
+++ hippocrat/src/pages/login/login.jsx
@@ -1,33 +1,29 @@
 import React, { useState } from "react";
+import {useNavigate} from "react-router-dom"
 import httpClient from "../../utils/httpClient";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import "./login.scss";
 
 const Login = () => {
+  const navigate = useNavigate()
   const [data, setData] = useState("");
   const [password, setPassword] = useState("");
 
   const loginUser = (e) => {
     e.preventDefault();
     (async () => {
-      if (!data) {
-        alert("please enter username or email");
-      } else if (!password) {
-        alert("please enter password");
-      } else {
-        try {
-          await httpClient.post("//localhost:5000/api/v1/login", {
-            data,
-            password,
-          });
-          // console.log(response);
-          window.location.href = "/profile";
-        } catch (e) {
-          // console.log(e)
-          if (Object.hasOwn(e, "response")) alert(e.response.data.error);
-          else alert("Server down. Please try again after sometime");
-        }
+      try {
+        await httpClient.post("//localhost:5000/api/v1/login", {
+          data,
+          password,
+        });
+        // console.log(response);
+        navigate("/profile");
+      } catch (e) {
+        // console.log(e)
+        if (Object.hasOwn(e, "response")) alert(e.response.data.error);
+        else alert("Server down. Please try again after sometime");
       }
     })();
   };
@@ -37,7 +33,6 @@ const Login = () => {
       <div className="login-form">
         <form onSubmit={loginUser}>
           <fieldset>
-            <legend>Login</legend>
             <label>
               <span>Username/Email:</span>
               <input
@@ -60,7 +55,7 @@ const Login = () => {
                 required
               />
             </label>
-            <input type="submit" className="submit" value="Login" />
+            <input type="submit" className="button" value="Login" />
           </fieldset>
         </form>
       </div>
