adc65fedf6495b9a0ac3c6ffab522133367d4ca2
xilder <olabowaleayobami@gmail.com>
2024-03-24 02:55:30 +0100

    adds the quiz path

--- hippocrat/src/App.jsx
+++ hippocrat/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/home/home";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import Profile from "./pages/profile/profile";
+import Admin from "./pages/admin/admin";
+import Quiz from "./pages/quiz/quiz";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        {/* <Route path="/admin" element={<Admin />} /> */}
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </Router>
   );
